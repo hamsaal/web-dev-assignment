f45cbd6eea2d1e2bd0e08bbd9332b4d83b8b58b7
hamsaal <hs8403340@gmail.com>
2024-05-20 18:10:49 +0200

    update

--- index.js
+++ index.js
@@ -6,3 +6,13 @@ document.addEventListener("DOMContentLoaded", function () {
     navLinks.classList.toggle("show");
   });
 });
+
+document.addEventListener('DOMContentLoaded', function() {
+  document.getElementById('exploreButton').addEventListener('click', function() {
+    window.location.href = 'explore.html';
+  });
+
+  document.getElementById('createButton').addEventListener('click', function() {
+    window.location.href = 'create.html';
+  });
+});
